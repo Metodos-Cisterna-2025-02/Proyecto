3e1975e9a93b9e25810d673d86bcdfe2a33f4443
Antony Luis P <antony.luis@usach.cl>
2026-01-23 02:05:06 -0300

    Update main_escenario.c

--- src/main_escenario.c
+++ src/main_escenario.c
@@ -1,52 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+int altura_valida(int escenario[8][8]){
+    int i, j, diferencia_altura = 0, es_valido;
+    for(i=0; i<8; i++){
+        for(j=0; j<8; j++){
+            es_valido = 0;
+            //Mira la diferencia de altura con la casilla de arriba
+            if(i>0){
+                diferencia_altura = abs(escenario[i][j] -escenario[i-1][j]);
+                if(diferencia_altura == 1)
+                    es_valido = 1;
+            }
+            //Mira la diferencia de altura con la casilla de abajo
+            if(i<7){
+                diferencia_altura = abs(escenario[i][j] -escenario[i+1][j]);
+                if(diferencia_altura == 1)
+                    es_valido = 1;
+            }
+            //Mira la diferencia de altura con la casilla izquierda
+            if(j>0){
+                diferencia_altura = abs(escenario[i][j] -escenario[i][j-1]);
+                if(diferencia_altura == 1)
+                    es_valido = 1;
+            }
+            //Mira la diferencia de altura con la casilla derecha
+            if(j<7){
+                diferencia_altura = abs(escenario[i][j] -escenario[i][j+1]);
+                if(diferencia_altura == 1)
+                    es_valido = 1;
+            }
+            if(es_valido == 0)
+                return 0;
+        }
+    }
+    return 1;
+}
 void generar_escenario(int escenario[8][8]){
     int i, j, referencia;
     for(i=0; i<8; i++){
         for(j=0; j<8; j++){
-            if(i == 0 && j == 0){
-                escenario[0][0] = rand()%6;    //primera casilla del escenario con altura aleatoria
+            if((i<4 && j<4) || (i>=4 && j>=4)){
+                escenario[i][j] = rand()%6;
             }
             else{
-                if(j>0){
-                    referencia = escenario[i][j-1];    //si no es la primera columna, toma de referencia la casilla izquierda
+                if((i==0 && j==4) || (i==4 && j==0)){
+                    escenario[i][j] = rand()%6;
                 }
                 else{
-                    referencia = escenario[i-1][j];    //si es la primera columna, toma de referencia la casilla de arriba
-                }
-
-                if(referencia == 0){    
-                    escenario[i][j] = 1;
-                }
-                else if(referencia == 5){
-                    escenario[i][j] = 4;
-                }
-                else{
-                    if(rand()%2 == 0){
-                        escenario[i][j] = referencia + 1;
+                    if(j>0 && j!=4){
+                        referencia = escenario[i][j-1];
                     }
                     else{
-                        escenario[i][j] = referencia - 1;
+                        referencia = escenario[i-1][j];
+                    }
+                    if(referencia == 0){
+                        escenario[i][j] = 1;
+                    }
+                    else if(referencia == 5){
+                        escenario[i][j] = 4;
+                    }
+                    else{
+                        if(rand()%2 == 0){
+                            escenario[i][j] = referencia + 1;
+                        }
+                        else{
+                            escenario[i][j] = referencia - 1;
+                        }
                     }
                 }
             }
         }
     }
 }
-
 void coordenadas_inicio(int *x, int *y, int x_meta, int y_meta){
     int distancia_valida = 0;
-    //mientras no haya una distancia valida, se generan nuevas coordenadas repetidamente
-    while(distancia_valida == 0){   
-        *x = rand()%8;    //genera coordenada x aleatoria para el jugador o IA
+    while(distancia_valida == 0){
+        *x = rand()%8;
         if(*x == 0 || *x == 7){
-            *y = rand()%8;   //si x es borde superior o inferior, genera coordenada y aleatoria
+            *y = rand()%8;
         }
         else{
-            int a = rand()%2;  //si x no es borde superior o inferior, genera coordenada y en uno de los bordes laterales
-            if(a==0){     
+            int a = rand()%2;
+            if(a==0){
                 *y = 0;
             }
             else{
@@ -55,20 +91,20 @@ void coordenadas_inicio(int *x, int *y, int x_meta, int y_meta){
         }
         int distancia_x = abs(*x - x_meta);
         int distancia_y = abs(*y - y_meta);
-        if((distancia_x + distancia_y) >= 4){    //verifica que la distancia directa a la meta sea como minimo 4
+        if((distancia_x + distancia_y) >= 4){
             distancia_valida = 1;
-        }//si cumple modifica las coordenadas declaradas en main
+        }
     }
 }
-
 
 int main(){
     srand(time(NULL));
     int escenario[8][8];
-    int i,j;
-    int altura;
-    generar_escenario(escenario);
-    //Define las variables para las coordenadas
+    int i, j;
+    do{
+        generar_escenario(escenario);
+    }
+    while(altura_valida(escenario) == 0);
     int x_jugador, y_jugador, x_ia, y_ia, x_meta, y_meta;
     //Se genera las coordenadas de la meta
     x_meta = rand()%8;
@@ -76,13 +112,11 @@ int main(){
     //Genera las coordenadas de inicio del jugador
     coordenadas_inicio(&x_jugador, &y_jugador, x_meta, y_meta);
     //Genera las coordenadas de inicio de la IA
-    coordenadas_inicio(&x_ia, &y_ia, x_meta, y_meta);
     //Si las coordenadas de ambos son iguales, cambia la posicion de la IA
-    if(x_ia == x_jugador && y_ia == y_jugador){
-        while(x_ia == x_jugador && y_ia == y_jugador){
-            coordenadas_inicio(&x_ia, &y_ia, x_meta, y_meta);
-        }
+    do{
+    coordenadas_inicio(&x_ia, &y_ia, x_meta, y_meta);
     }
+    while(x_ia == x_jugador && y_ia == y_jugador);
     //Muestra escenario
     printf("\nESCENARIO\n\n");
     for(i=0; i<8; i++){
@@ -97,4 +131,3 @@ int main(){
     printf("Posicion meta: %d , %d", x_meta, y_meta);
     return 0;
 }
-
