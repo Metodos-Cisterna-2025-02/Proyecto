cde3eb556e0a204819a1413fc50fdffe1369cc3c
Antony Luis P <antony.luis@usach.cl>
2026-01-23 05:57:34 -0300

    mod io.h

--- src/headers/io.h
+++ src/headers/io.h
@@ -1,39 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#ifndef IO_H
+#define IO_H
 
-/* Logs related declarations */
-
-/* Map related declarations */
-#define MAX_VERTICAL 32
-#define MAX_HORIZONTAL 32
-#define MAX_HEIGHT 5
-#define MIN_HEIGHT 0
-#define MAP_TEST "maps/map_test.txt"
-#define MAP_TEST_SIZE 8
+#include "definiciones.h"
 
 /* Map related variables */
+
 static FILE* mapFile;
 static int* map[MAX_VERTICAL];
 static int mapLoaded = 0;
 static char buffer[8];
 
-/* Map related function prototypes */
+
+/* Interface related variables */
+
+static FILE* interfaceFile;
+static int interfaceLoaded = 0;
+static char interfaceBuffer[MAX_LINE_LENGTH];
+
+
+/* Prototipos de funciones estáticas */
 static void readMap();
-int** writeMap(int x, int y, int value);
-int** getMap();
-void printMap();
+static void readInterface();
+static void updateInterface(int x, int y, int h);
 
+/* Definiciones de Funciones de Mapa */
 
-/* Maps related functions */
+/* Lee el archivo del mapa y lo guarda como una matriz de enteros */
+static void readMap() {
+	int i;
+	int j;
 
-/* Retorna el mapa como una matriz de NxN enteros */
+	mapFile = fopen(MAP_TEST, "r");
+
+	if (!mapFile) {
+		perror("Error al abrir el archivo");
+		return;
+	}
+
+	for (i = 0; i < MAP_TEST_SIZE; i++) {
+	map[i] = (int*) calloc(MAX_HORIZONTAL, sizeof(int));
+		if (!map[i]) {
+			perror("Error al alocar memoria para fila");
+			fclose(mapFile);
+			return;
+		}
+	}
+
+	for (i = 0; i < MAP_TEST_SIZE; i++) {
+		for (j = 0; j < MAP_TEST_SIZE; j++) {
+			fscanf(mapFile, "%d", &map[i][j]); /* valor (i,j) a map[i][j] */
+			int current;
+			current = snprintf(buffer, floor(MAX_HEIGHT / 10) + 2, "%d", map[i][j]);
+			if (current < MIN_HEIGHT || current > MAX_HEIGHT) { 
+				map[i][j] = 0; /* valor por defecto en caso de no cumplir condicion de altura */
+			}
+		}
+	}
+
+	fclose(mapFile);
+}
+
+/* Retorna el mapa como una matriz de NxM enteros */
 int** getMap() {
-    if (!mapLoaded)
-        readMap();
-    
-    return map;
+	if (!mapLoaded)
+		readMap();
+
+	return map;
 }
 
 /* Escribe un valor de altura en la posicion (X, Y) en el mapa */
@@ -69,38 +101,36 @@ void printMap() {
 	}
 }
 
+/* Liberar memoria al final del juego */ 
+void freeMap() {
+	if (mapLoaded)
+		for (int i = 0; i < MAP_TEST_SIZE; i++)
+			free(map[i]);
+	mapLoaded = 0;
+}
 
-/* Lee el archivo del mapa y lo guarda como una matriz de enteros */
-static void readMap() {
-	int i;
-	int j;
 
-	mapFile = fopen(MAP_TEST, "r");
+/* Definiciones de Funciones de Interfaz */
 
-	if (!mapFile) {
-		perror("Error al abrir el archivo");
+/* Lee el archivo de la interfaz */
+static void readInterface() {
+	interfaceFile = fopen(INTERFACE_FILE, "r");
+
+	if (!interfaceFile) {
+		perror("Error al abrir el archivo de la interfaz");
 		return;
 	}
 
-	for (i = 0; i < MAP_TEST_SIZE; i++) {
-        map[i] = (int*) calloc(MAX_HORIZONTAL, __SIZEOF_INT__);
-		if (!map[i]) {
-			perror("Error al alocar memoria para fila");
-			fclose(mapFile);
-			return;
-		}
-	}
-
-	for (i = 0; i < MAP_TEST_SIZE; i++) {
-		for (j = 0; j < MAP_TEST_SIZE; j++) {
-			fscanf(mapFile, "%d", &map[i][j]); /* valor (i,j) a map[i][j] */
-			int current;
-			current = snprintf(buffer, round(MAX_HEIGHT / 10) + 2, "%d", map[i][j]);
-			if (current < MIN_HEIGHT || current > MAX_HEIGHT) { 
-				map[i][j] = 0; /* valor por defecto en caso de no cumplir condicion de altura */
-			}
-		}
-	}
-
-	fclose(mapFile);
+	interfaceLoaded = 1;
 }
+
+/* Actualiza la interfaz en la posicion (X, Y, H) */
+void updateInterface(int x, int y, int h) {
+	if (!interfaceLoaded)
+		readInterface();
+
+	
+
+}
+
+#endif
